afd2a203d78475738d422b4e96f820ff4c25e6e0
marjimleo <mariosevillista002@gmail.com>
2026-05-24 20:59:09 +0200

    feat: Implementación de GridSearchCV para la elección de hiperparámetros, y los correspondientes hiperparámetros

--- TFG_Chollos/Modelizacion/Modelizacion.py
+++ TFG_Chollos/Modelizacion/Modelizacion.py
@@ -2,9 +2,23 @@
 preprocessing.py
 ====================
 Dado el volumen del dataset, se optó por una partición holdout 70/15/15. Sobre el conjunto de entrenamiento se aplicó validación cruzada K-Fold (K=5) mediante GridSearchCV para el ajuste de hiperparámetros de cada modelo de forma independiente. Posteriormente, el conjunto de validación se empleó para la comparación y selección del modelo final entre los candidatos ya optimizados. El conjunto de test permaneció intacto hasta la evaluación final, garantizando una estimación del rendimiento libre de sesgos.
-En resumen, hacemos una combinación entre partición train-validation-test y Cross-Validation con 5 folds.
+En resumen, hacemos una combinación entre partición train-validation-test y Cross-Validation con cv=5 folds.
+
+ Lo que hace internamente con cv=5:
+
+  X_train  →  fold1 | fold2 | fold3 | fold4 | fold5
+                ↓
+  Para cada combinación de hiperparámetros:
+    entrena en 4 folds → evalúa en el fold restante → repite 5 veces → saca media
+                ↓
+  Se queda con la combinación que mejor media obtiene
 
 Dado que la mayoría de los algoritmos de aprendizaje automático operan sobre espacios vectoriales numéricos, las variables categóricas presentes en el dataset fueron codificadas mediante get_dummies y LabelEncoder.
+
+Este va a ser el flujo de trabajo completo:
+  X_train  →  entrenar cada modelo
+  X_val    →  ajustar hiperparámetros + elegir el mejor modelo   ← tomas decisiones
+  X_test   →  evaluar el modelo elegido y reportar               ← no tomas decisiones
 
 Dependencias:
     - Python >= 3.10
@@ -34,6 +48,7 @@ import matplotlib.pyplot as plt
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
 from sklearn import linear_model
 from sklearn.tree import DecisionTreeRegressor, plot_tree
 from sklearn.ensemble import RandomForestRegressor
@@ -185,60 +200,142 @@ def crear_regresion_lineal(X_train_est, X_val_est, X_test_est, y_train_est, y_va
 
 
 
-def crear_arbol_decision(features_train:pd.DataFrame, target_train:pd.Series):
+def crear_arbol_decision(conjunto_entrenamiento:pd.DataFrame, target_entrenamiento:pd.Series):
     '''
-    No es necesario escalar los datos porque solo pregunta "¿es X mayor que umbral?", la magnitud no importa
+    No es necesario escalar los datos porque solo pregunta "¿es X mayor que umbral?", la magnitud no importa.
+    Hay que decidir entre estos hiperparámetros: 
+        # max_depth : Profundidad máxima del árbol. Sin límite → overfitting. Valor bajo → underfitting   
+        min_samples_split : Mínimo de muestras que debe tener un nodo para poder dividirse. Se dejaron los valores por defecto por ser suficientemente robustos
+        min_samples_leaf :  Mínimo de muestras que debe tener una hoja (nodo final). Evita hojas con 1 sola muestra. Se dejaron los valores por defecto por ser suficientemente robustos
     '''
 
-    arbol = DecisionTreeRegressor()   #Creamos el modelo
-    arbol.fit(features_train, target_train)   #Entrenamos el modelo con nuestros datos
+    arbol = DecisionTreeRegressor(random_state=42)   #Creamos el modelo
+    param_grid = {
+      'max_depth': [3, 5, 10, 15, None],
+    }
+
+    grid_search = GridSearchCV(
+      estimator=arbol,
+      param_grid=param_grid,
+      cv=5,                          # K-Fold con 5 folds, sobre X_train
+      scoring='neg_mean_squared_error',  # métrica a optimizar
+      n_jobs=-1                      # usa todos los núcleos del procesador
+    )
+    grid_search.fit(conjunto_entrenamiento, target_entrenamiento)   #Entrenamos el modelo con nuestros datos
     logger.info('✅ Creado y entrenado el modelo de "Árbol de Decisión" correctamente')
+
+    mejor_arbol = grid_search.best_estimator_
+    print(grid_search.best_params_)    # {'max_depth': 5}
 
     plt.figure(figsize=(20, 10))
     plot_tree(
-        decision_tree = arbol,    #Representamos el árbol
+        decision_tree = mejor_arbol,    #Representamos el árbol
         filled=True,   #Añadimos colores a los valores predichos en cada nodo. filled no colorea cada valor individual colorea cada nodo del árbol. Coge la media de todas las muestras que caen en ese nodo, y cuanto más se aleje del promedio, más intenso el color
         max_depth=3,    #Con max_depth=3 vemos las primeras decisiones más importantes (las que más reducen el error)
         fontsize=9,    #Se ven muy pequeñas las letras
-        feature_names=features_train.columns.to_list())   #Añadimos los nombres de las variables predictoras
+        feature_names=conjunto_entrenamiento.columns.to_list())   #Añadimos los nombres de las variables predictoras
     plt.show()
     logger.info('✅ Representado el "Árbol de Decisión" correctamente')
-    
-    return arbol
 
-def crear_bosque_aleatorio():
+    #  Después, usas X_val para comparar modelos:
+    # y_pred_val = mejor_arbol.predict(X_val)
+    # print(r2_score(y_val, y_pred_val))
+    # print(mean_squared_error(y_val, y_pred_val))
+    
+    return mejor_arbol
+
+def crear_bosque_aleatorio(conjunto_entrenamiento:pd.DataFrame, conjunto_validacion:pd.DataFrame, conjunto_test:pd.DataFrame, target_entrenamiento:pd.DataFrame, target_validacion:pd.DataFrame, target_test:pd.DataFrame):
     '''
-    No es necesario escalar los datos porque solo pregunta "¿es X mayor que umbral?", la magnitud no importa
+    No es necesario escalar los datos porque solo pregunta "¿es X mayor que umbral?", la magnitud no importa.
+    Hay que decidir entre estos hiperparámetros: 
+        # n_estimators : Número de árboles del bosque. Más árboles → más estable, más lento  
+        # max_depth :  Igual que en el árbol individual
+        max_features : Cuántas features considera en cada división. Introduce aleatoriedad para que los árboles sean distintos entre sí. Se dejaron los valores por defecto por ser suficientemente robustos
     '''
-    forest = RandomForestClassifier()
-    forest.fit(X_train, y_train)
+    bosque = RandomForestRegressor(random_state=42)
+    param_grid = {
+      'n_estimators': [50, 100, 200],
+      'max_depth':    [5, 10, 20, None],
+    }
+    grid_search = GridSearchCV(
+      estimator=bosque,
+      param_grid=param_grid,
+      cv=5,                          # K-Fold con 5 folds, sobre X_train
+      scoring='neg_mean_squared_error',  # métrica a optimizar
+      n_jobs=-1                      # usa todos los núcleos del procesador
+    )
+    grid_search.fit(conjunto_entrenamiento, target_entrenamiento)
+
+    mejor_bosque = grid_search.best_estimator_
+    print(grid_search.best_params_)    # {'max_depth': 5}
+
+    #  Después, usas X_val para comparar modelos:
+    # y_pred_val = mejor_arbol.predict(X_val)
+    # print(r2_score(y_val, y_pred_val))
+    # print(mean_squared_error(y_val, y_pred_val))
+
+    mejor_bosque.score(conjunto_test, target_test)  #Devuelve el porcentaje de predicciones correctas en clasificación, y la precisión o R^2 en regresión
+
+    return mejor_bosque
 
 
 def crear_maquinas_vectores_soporte(X_train_est, X_val_est, X_test_est, y_train_est, y_val_est, y_test_est):
     '''
-    Estandarizamos los datos porque SVM maximiza márgenes → sensible a la magnitud
+    Estandarizamos los datos porque SVM maximiza márgenes → sensible a la magnitud.
+    Hay que decidir entre estos hiperparámetros: 
+        # C : Penalización por errores. C alto → intenta no fallar ningún punto (riesgo de overfitting). C bajo → acepta más errores a cambio de un margen más amplio
+        # kernel : Función que transforma los datos (linear, rbf, poly). Permite separar datos no lineales 
+        gamma : Controla el radio de influencia de cada punto. Solo aplica a kernels rbf y poly. Mencionar si solo si uso kernel rbf, si no, ni lo menciono
     '''
-
+    param_grid = {
+      'C':      [0.1, 1, 10, 100],
+      'kernel': ['linear', 'rbf'],
+    }
 
 
 def crear_k_vecinos_cercanos(X_train_est, X_val_est, X_test_est, y_train_est, y_val_est, y_test_est):
     '''
-    Estandarizamos los datos porque KNN mide distancia entre puntos → una variable en miles de euros dominaría sobre una en m²
+    Estandarizamos los datos porque KNN mide distancia entre puntos → una variable en miles de euros dominaría sobre una en m².
+    Hay que decidir entre estos hiperparámetros: 
+        # k (número de vecinos) : Número de vecinos que consulta para predecir. k=1 → muy sensible al ruido. k grande → predicciones más suavizadas 
+        weights : Si todos los vecinos pesan igual (uniform) o los más cercanos pesan más (distance) 
+        metric : Cómo mide la distancia entre puntos (euclidean, manhattan...)
     '''
-
+    param_grid = {
+      'n_neighbors': [3, 5, 7, 10, 15],
+    }
 
 
 def crear_redes_neuronales(X_train_norm, X_val_norm, X_test_norm, y_train_norm, y_val_norm, y_test_norm):
     '''
-    Normalizamos los datos porque las Redes Neuronales aprenden con gradientes → convergen mejor con datos en [0,1]
+    Normalizamos los datos porque las Redes Neuronales aprenden con gradientes → convergen mejor con datos en [0,1].
+    Hay que decidir el 'early stopping' y entre estos hiperparámetros: 
+        # hidden_layer_sizes : Número de capas ocultas y neuronas por capa  
+        # learning_rate : Cuánto ajusta los pesos en cada paso. Alto → aprende rápido pero puede no converger. Bajo → aprende despacio pero más estable
+        # epochs / max_iter : Número de veces que recorre todo el dataset entrenando. Se pone un valor alto y se delega en early stopping 
     '''
+    # Redes Neuronales (MLPRegressor de sklearn)
+    param_grid = {
+      'hidden_layer_sizes': [(64,), (128,), (64, 64), (128, 64)],   #(64,) → 1 capa oculta con 64 neuronas, (64, 64) → 2 capas ocultas con 64 neuronas cada una
+      'learning_rate_init': [0.001, 0.01, 0.1],
+    }
 
 
 
 def crear_boosting():
     '''
-    No es necesario escalar los datos porque solo pregunta "¿es X mayor que umbral?", la magnitud no importa
+    No es necesario escalar los datos porque solo pregunta "¿es X mayor que umbral?", la magnitud no importa.
+    Hay que decidir el 'early stopping' y entre estos hiperparámetros: 
+        # learning_rate : Número de árboles encadenados. Más árboles → más preciso, pero más riesgo de overfitting 
+        # n_estimators : Cuánto contribuye cada árbol nuevo. Valores bajos necesitan más árboles   
+        # max_depth : Profundidad de cada árbol individual. Suelen ser árboles poco profundos (3-5) 
     '''
+    # Boosting (GradientBoostingRegressor de sklearn)
+    param_grid = {
+      'n_estimators':  [50, 100, 200],
+      'learning_rate': [0.01, 0.05, 0.1],
+      'max_depth':     [3, 5, 7],
+    }
 
 
 # =============================================================================
