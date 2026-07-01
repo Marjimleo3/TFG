836d5f6046e86f754983e0352a19fbc07d52a5ec
marjimleo <mariosevillista002@gmail.com>
2026-07-01 20:33:02 +0200

    feat: Add número de correlación en el corrplot de R

--- TFG_Chollos/Graficos/correlacion.R
+++ TFG_Chollos/Graficos/correlacion.R
@@ -6,5 +6,6 @@ cor_matrix <- cor(as.data.frame(lapply(df, as.numeric)), use = "pairwise.complet
 
 png("images/correlacion_lineal.png", width = 1800, height = 1600, res = 150)
 corrplot(cor_matrix, method = "color", type = "lower", tl.cex = 0.7, tl.srt = 45,
-         title = "Correlación lineal (Pearson)", mar = c(0, 0, 2, 0))
+         title = "Correlación lineal (Pearson)", mar = c(0, 0, 2, 0),
+         addCoef.col = "black", number.cex = 0.45)
 dev.off()
